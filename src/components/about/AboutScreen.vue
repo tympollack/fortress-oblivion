00ddb431f6d2804522655b2e1a3bf3df8f81c326
Tym Pollack <tympollack@gmail.com>
2020-08-29 15:30:20 -0400

    add about content

--- src/components/about/AboutScreen.vue
+++ src/components/about/AboutScreen.vue
@@ -1,5 +1,33 @@
 <template>
-  <div>about placeholder</div>
+  <div>
+    <h2>Fortress Oblivion</h2>
+    <h3>A Star Realms Dungeon Crawl Adventure</h3>
+    <h4>Goal: Climb to the top of Fortress Oblivion.</h4>
+    <h4>Win an encounter to get a key or potion.</h4>
+    <h4>Use a key to climb to the next floor.</h4>
+    <h4>Losing an encounter costs health.</h4>
+    <h4>Use a potion to heal yourself.</h4>
+    <h4>Return to the village for a full reset.</h4>
+    <h4>Don't die.</h4>
+
+    <p>
+      you are not allowed to artificially extend an counter by
+      withholding lethal damage or by not playing damage cards
+      at end game to set up a bigger hit. however, discarding cards for
+      deck control during the match is permissible for the sake of
+      trying to win. be sporting, and don't concede.
+    </p>
+
+    <p>
+      resting in the fortress takes 90 minutes per hit point, with a
+      minimum rest of 3 hours. return to the village takes 3.5
+      hours. healing once at the village is 6 hit points per hour.
+      siphoning and drinking a potion each take 1 minute per hit point.
+      climbing to the next floor takes 15 minutes. searching for
+      treasure takes 5 minutes. potion, key, and equipment is lost
+      upon death.
+    </p>
+  </div>
 </template>
 
 <script>
